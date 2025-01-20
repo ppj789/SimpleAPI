a00c71febf2c106f8537337196714b28d1864cc1
Jason Werner <jason.werner@dariel.co.za>
2025-01-20 09:50:50 +0200

    Restarting, due to misreading the assignment

--- Program.cs
+++ Program.cs
@@ -20,92 +20,13 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    // Define API Key Authentication
-    options.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "X-API-KEY",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Custom API key for accessing the API"
-    });
+builder.Services.AddSwaggerGen();
 
-    // Define JWT Bearer Token Authentication
-    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Enter 'Bearer' followed by your token. Example: 'Bearer abc123xyz'"
-    });
 
-    // Set up the security requirement for either ApiKey or Bearer token
-    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "ApiKey"
-                }
-            },
-            new string[] { } // No scopes required for API key
-        },
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { } // No scopes required for JWT
-        }
-    });
-});
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = "your_issuer",
-        ValidAudience = "your_audience",
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication"))
-    };
-});
-builder.Services.AddAuthentication("ApiKey")
-        .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>("ApiKey", null);
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ApiKeyOrBearer", policy =>
-    {
-        policy.AddAuthenticationSchemes("Bearer", "ApiKey");
-        policy.RequireAuthenticatedUser();
-    });
-});
+builder.Services.AddAuthorization();
 
-builder.Services.AddControllers(options =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-    options.Filters.Add(new AuthorizeFilter(policy));
-});
 
 var app = builder.Build();
 
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+﻿TODO:
+
+1, just stick to API key (misread the question)
+2, implement a controller -> service -> repository services 
+3. add testing
